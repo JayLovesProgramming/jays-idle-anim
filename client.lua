ffd0a64b774cb32a5a952fc2c97b7bc73b6b342d
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-10 00:53:49 +0000

    fixed some issues and added qb/qbx intergration
    STILL A WIP

--- client.lua
+++ client.lua
@@ -1,8 +1,10 @@
 local config = require "config"
 local isIdlePlaying = false
 local lastActionTime = 0
+local idleTimeout = tonumber(config.idleTimeout)
+local randomIdleAnim = config.randomIdleAnimations
 local lastIdleAnimation = ""
-local idleTimeout = config.idleTimeout
+
 local function cancelEmote()
     if exports.scully_emotemenu:isInEmote() then
         exports.scully_emotemenu:cancelEmote()
@@ -11,69 +13,83 @@ end
 
 local function playRandomIdleAnimation()
     local availableAnimations = {}
-    for _, anim in ipairs(config.randomIdleAnimations) do
+    for _, anim in ipairs(randomIdleAnim) do
         if anim ~= lastIdleAnimation then
             table.insert(availableAnimations, anim)
         end
     end
+
     if #availableAnimations > 0 then
         local randomIndex = 1 + math.floor(#availableAnimations * math.random())
         local randomisedAnim = availableAnimations[randomIndex]
         exports.scully_emotemenu:playEmoteByCommand(randomisedAnim)
         isIdlePlaying = true
         lastIdleAnimation = randomisedAnim
-        if config.debug then
-          print("Playing idle animation:", randomisedAnim)
-        end
-        Wait(idleTimeout-4000)
+        print("Playing idle animation:", randomisedAnim)
+    end
+end
+
+
+
+local function handleKeybindRelease()
+    if IsPedWalking(cache.ped) then
+        lastActionTime = GetGameTimer()
+    elseif GetGameTimer() - lastActionTime > idleTimeout  then
         cancelEmote()
     end
 end
 
-local function handleKeybindRelease()
-    if IsPedWalking(cache.ped) then
-      lastActionTime = GetGameTimer()
-    elseif GetGameTimer() - lastActionTime > idleTimeout  then
-      cancelEmote()
-      if config.debug then
-        print("Cancelled emote")
-      end
-    end
-end
 
 CreateThread(function()
-    lib.addKeybind({
-        name = 'IdleAnimation',
-        description = 'Idle animation',
-        defaultKey = 'S',
-        onReleased = handleKeybindRelease
-    })
-    while true do
-        Wait(2000)
-        -- if config.debug then
-        --   print("Is in emote?", exports.scully_emotemenu:isInEmote())
-        --   Wait(2000)
-        --   print("Is in vehicle?", GetVehiclePedIsIn(cache.ped, false))
-        --   Wait(2000)
-        --   print("Does entity exist?", DoesEntityExist(cache.ped))
-        --   Wait(2000)
-        --   print("Is ped still??", IsPedStill(cache.ped))
-        --   Wait(2000)
-        -- end
-        if DoesEntityExist(cache.ped) and GetVehiclePedIsIn(cache.ped, false) == 0  and not IsEntityDead(cache.ped) and IsPedStill(cache.ped) and not exports.scully_emotemenu:isInEmote() then
-            if GetGameTimer() - lastActionTime > idleTimeout  then
-              if config.debug then
-                print("Triggered play animation function")
+    if config.usingQB then 
+RegisterNetEvent("QBCore:Client:OnPlayerLoaded", function()
+  Wait(15000)
+  CreateThread(function()
+      local keybind = lib.addKeybind({
+          name = 'IdleAnimation',
+          description = 'Idle animation',
+          defaultKey = 'S',
+          onReleased = handleKeybindRelease
+      })
+      while true do
+          Wait(idleTimeout)
+          if DoesEntityExist(cache.ped)
+          and not GetVehiclePedIsIn(cache.ped, false) ~= 0 
+          and not IsEntityDead(cache.ped) 
+          and IsPedStill(cache.ped) 
+          and not exports.scully_emotemenu:isInEmote() 
+          and not IsPedWalking(cache.ped) then
+              if GetGameTimer() - lastActionTime > idleTimeout  then
+                  print("Playing IDLE Anim")
+                  playRandomIdleAnimation()
               end
-                playRandomIdleAnimation()
-            end
-        end
-    end
+          end
+      end
+  end)
 end)
-
-AddEventHandler("onResourceStop", function(resourceName)
-    if resourceName == "jays-idle-anim" then
-      Wait(1000)
-      cancelEmote()
-    end
+else
+  Wait(20000)
+  CreateThread(function()
+      local keybind = lib.addKeybind({
+          name = 'IdleAnimation',
+          description = 'Idle animation',
+          defaultKey = 'S',
+          onReleased = handleKeybindRelease
+      })
+      while true do
+          Wait(idleTimeout)
+          if DoesEntityExist(cache.ped)
+          and not GetVehiclePedIsIn(cache.ped, false) ~= 0 
+          and not IsEntityDead(cache.ped) 
+          and IsPedStill(cache.ped) 
+          and not exports.scully_emotemenu:isInEmote() 
+          and not IsPedWalking(cache.ped) then
+              if GetGameTimer() - lastActionTime > idleTimeout  then
+                  print("Playing IDLE Anim")
+                  playRandomIdleAnimation()
+              end
+          end
+      end
+  end)
+end
 end)
