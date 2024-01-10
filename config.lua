672b26a7a9d77fcab614bf21600c7309e5cd9cbb
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-10 00:54:23 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,13 @@
 return {
-  debug = false,
-  idleTimeout = 13000,
+  debug = true,
+  idleTimeout = 12000,
+  usingQB = true, -- Set to false if you are not using qb-core or qbx_core
   randomIdleAnimations = {
-    "idle3",
-    "idle4",
-    "idle7", 
-    "idle8", 
-    "idle9", 
-    "handsinpockets2"
+  "idle3",
+  "idle4",
+  "idle7", 
+  "idle8", 
+  "idle9", 
+  "handsinpockets2"
   },
 }
