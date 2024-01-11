f4bda3f6460845fa01ef95b36433e1aa67e31bec
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-11 20:07:29 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,15 @@
-return {
+config = {
   debug = false,
   emoteMenu = "scully",  -- scully and rpemotes (THESE ARE THE ONLY OPTIONS FOR NOW)
   idleTimeout = 10000,
   usingQB = true,
   randomIdleAnimations = {
-  "idle3",
-  "idle4",
-  "idle7", 
-  "idle8", 
-  "idle9", 
-  "handsinpockets2"
+    "idle3",
+    "idle4",
+    "idle7", 
+    "idle8", 
+    "idle9", 
+    "handsinpockets2"
   },
 }
+
