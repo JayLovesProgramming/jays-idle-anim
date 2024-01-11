4af763fb18d3e8bef49c4d7f995c5b6009b9a918
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-11 20:02:25 +0000

    removed ox_lib dependency and added a more fluent way to cancel idle emote

--- client.lua
+++ client.lua
@@ -1,9 +1,9 @@
 local config = require "config"
-local randomIdleAnim = config.randomIdleAnimations
-local lastIdleAnimation
+local lastIdleAnimation = nil
 local isIdlePlaying = false
 local lastActionTime = 0
-local idleTimeout = tonumber(config.idleTimeout)
+local randomIdleAnim = config.randomIdleAnimations
+local idleTimeout = config.idleTimeout
 
 local function cancelEmote()
     if config.debug then
@@ -39,93 +39,100 @@ local function playRandomIdleAnimation()
         isIdlePlaying = true
         lastIdleAnimation = randomisedAnim
         if config.debug then
-            print("Playing idle animation:", randomisedAnim)
+            print("[DEBUG] Playing idle animation:", randomisedAnim)
         end
     end
 end
 
 local function handleKeybindRelease()
-    if IsPedWalking(cache.ped) or not IsPedStill(cache.ped) then
+    if IsPedWalking(PlayerPedId()) or not IsPedStill(PlayerPedId()) then
         lastActionTime = GetGameTimer()
         if config.debug then
             print("[DEBUG] Ped isn't still so I haven't cancelled the emote")
         end
-    elseif GetGameTimer() - lastActionTime > idleTimeout  then
+    elseif GetGameTimer() - lastActionTime > idleTimeout then
         cancelEmote()
     end
 end
 
-
 CreateThread(function()
-if config.usingQB then 
-    AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
-    Wait(12000)
-    if config.debug then 
-        print("[DEBUG] jays-idle-anim script has started")
-    end
-    CreateThread(function()
-      local keybind = lib.addKeybind({
-          name = 'IdleAnimation',
-          description = 'Idle animation',
-          defaultKey = 'S',
-          onReleased = handleKeybindRelease
-      })
-      while true do
-          Wait(idleTimeout)
-          if DoesEntityExist(cache.ped)
-          and not GetVehiclePedIsIn(cache.ped, false) ~= 0 
-          and not IsEntityDead(cache.ped) 
-          and IsPedStill(cache.ped) 
-          and not IsPedWalking(cache.ped) then
-              if GetGameTimer() - lastActionTime > idleTimeout  then
-                if config.debug then
-                    print("Playing IDLE Anim")
-                end
-            if config.emoteMenu == "rpemotes" then
-                if not exports["rpemotes"]:IsPlayerInAnim() then
-                    playRandomIdleAnimation()
-                end
-            elseif config.emoteMenu == "scully" then
-                    if not exports.scully_emotemenu:isInEmote()  then
-                        playRandomIdleAnimation()
-                    end
-                end
-                end
-              end
-          end
-  end)
-end)
-elseif not config.usingQB and config.debug then
-  CreateThread(function()
-      local keybind = lib.addKeybind({
-          name = 'IdleAnimation',
-          description = 'Idle animation',
-          defaultKey = 'S',
-          onReleased = handleKeybindRelease
-      })
-      while true do
-          Wait(idleTimeout)
-          if DoesEntityExist(cache.ped)
-          and not GetVehiclePedIsIn(cache.ped, false) ~= 0 
-          and not IsEntityDead(cache.ped) 
-          and IsPedStill(cache.ped) 
-          and not IsPedWalking(cache.ped) then
-              if GetGameTimer() - lastActionTime > idleTimeout  then
-                if config.debug then
-                  print("Playing IDLE Anim")
-                  if config.emoteMenu == "rpemotes" then
-                    if not exports["rpemotes"]:IsPlayerInAnim() then
-                        playRandomIdleAnimation()
-                    end
-                elseif config.emoteMenu == "scully" then
-                    if not exports.scully_emotemenu:isInEmote()  then
-                            playRandomIdleAnimation()
+    if config.usingQB then 
+        if LocalPlayer.state.isLoggedIn then
+            Wait(10000) -- Small delay to stop it playing while in multicharacter // Leave this for now
+            if config.debug then 
+                print("[DEBUG] jays-idle-anim script has started")
+            end
+            CreateThread(function()
+                while true do
+                    Wait(idleTimeout)
+                    if DoesEntityExist(PlayerPedId())
+                    and not GetVehiclePedIsIn(PlayerPedId(), false) ~= 0 
+                    and not IsEntityDead(PlayerPedId()) 
+                    and IsPedStill(PlayerPedId()) 
+                    and not IsPedWalking(PlayerPedId()) then
+                        if GetGameTimer() - lastActionTime > idleTimeout  then
+                            if config.debug then
+                                print("[DEBUG] Trying to play animation")
+                            end
+                            if config.emoteMenu == "rpemotes" then
+                                if not exports["rpemotes"]:IsPlayerInAnim() then
+                                    playRandomIdleAnimation()
+                                end
+                            elseif config.emoteMenu == "scully" then
+                                if not exports.scully_emotemenu:isInEmote() then
+                                    playRandomIdleAnimation()
+                                end
+                            end
                         end
                     end
-              end
+                end
+            end)
+        end
+    else
+        CreateThread(function()
+            while true do
+                Wait(idleTimeout)
+                if DoesEntityExist(PlayerPedId())
+                and not GetVehiclePedIsIn(PlayerPedId(), false) ~= 0 
+                and not IsEntityDead(PlayerPedId()) 
+                and IsPedStill(PlayerPedId()) 
+                and not IsPedWalking(PlayerPedId()) then
+                    if GetGameTimer() - lastActionTime > idleTimeout  then
+                        if config.debug then
+                            print("[DEBUG] Trying to play animation")
+                        end
+                        if config.emoteMenu == "rpemotes" then
+                            if not exports["rpemotes"]:IsPlayerInAnim() then
+                                playRandomIdleAnimation()
+                            end
+                        elseif config.emoteMenu == "scully" then
+                            if not exports.scully_emotemenu:isInEmote()  then
+                                playRandomIdleAnimation()
+                            end
+                        end
+                    end
+                end
             end
-          end
-      end
-  end)
-end
+        end)
+    end
 end)
+
+-- Command --
+RegisterCommand('cancelidleanim', function()
+    handleKeybindRelease()
+end)
+
+-- Key Mappings --
+RegisterKeyMapping('cancelidleanim', 'Cancel Idle Animation (W)', 'keyboard', 'w')
+RegisterKeyMapping('cancelidleanim', 'Cancel Idle Animation (A)', 'keyboard', 'a')
+RegisterKeyMapping('cancelidleanim', 'Cancel Idle Animation (S)', 'keyboard', 's')
+RegisterKeyMapping('cancelidleanim', 'Cancel Idle Animation (D)', 'keyboard', 'd')
+
+-- When the resource restarts >> cancel emote pretty please --
+AddEventHandler("onResourceStop", function(resourceName) -- Used for when the resource is restarted while in game
+	if GetCurrentResourceName() ~= resourceName then
+        return 
+    end
+    cancelEmote()
+end)
+
