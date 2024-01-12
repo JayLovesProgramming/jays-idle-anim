145f790da8d1dd86f025d7c6dc8a619dd6fca569
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-12 15:54:48 +0000

    made better 😈

--- config.lua
+++ config.lua
@@ -1,9 +1,14 @@
 config = {
-  debug = false,
+  debug = false, -- Only used if you want to fix something / Mainly used by Jay when he fixes things
+  usingQB = true, -- Set to true if you are using QB/QBX or if you 
+  
   emoteMenu = "scully",  -- scully and rpemotes (THESE ARE THE ONLY OPTIONS FOR NOW)
-  idleTimeout = 10000,
-  usingQB = true,
-  randomIdleAnimations = {
+  idleTimeout = 3500, -- Cooldown for the idle animation
+
+  useSingleEmote = false, -- Set to true if you want to use only one animation and not a list of random ones
+  singleEmote = "idle4", -- Adjust this as you like and if useSingleEmote is set to true 
+
+  randomIdleAnimations = { -- Adjust this as you like and if useSingleEmote is set to false 
     "idle3",
     "idle4",
     "idle7", 
